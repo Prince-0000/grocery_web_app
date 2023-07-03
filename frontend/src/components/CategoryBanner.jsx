4c76a96ed489cf6547a46cfcb6b9fde0a72bc09c
Prince <princemehta516@gmail.com>
2023-07-03 17:00:07 +0530

    update files

--- frontend/src/components/CategoryBanner.jsx
+++ frontend/src/components/CategoryBanner.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import CategoryCards from './CategoryCards'
+import {client, urlFor} from '../lib/client';
 
 const CategoryBanner = () => {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const query = '*[_type == "category"]';
+        const response = await client.fetch(query);
+        
+        setState(response);
+      } catch (error) {
+        console.error("Error fetching banner data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if(state.length===0) return <div>loading...</div>
   return (
-    <div className='grid grid-flow-col auto-cols-min bg-white h-[90px] my-4 mx-4'>
+    <div className='grid grid-flow-col auto-cols-min bg-white h-[100px] my-4 mx-4'>
+      <CategoryCards name={state[0].name} image={urlFor(state[0].image).url()}/>
+      {/* <CategoryCards />
       <CategoryCards />
       <CategoryCards />
-      <CategoryCards />
-      <CategoryCards />
-      <CategoryCards />
+      <CategoryCards /> */}
+      {state.map((card)=>(
+          <div index={card._id}>
+            <CategoryCards name={card.name} image={urlFor(card.image).url()}/>
+          </div>
+      ))}
      
     </div>
   )
--- frontend/src/components/CategoryCards.jsx
+++ frontend/src/components/CategoryCards.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Speaker from '../assests/head.png';
-const CategoryCards = () => {
+import { urlFor } from '../lib/client';
+
+const CategoryCards = ({name, image}) => {
+
   return (
     <div className='flex items-center flex-col w-[100px] h-full mx-4 cursor-pointer'>
-        <div className='pt-[7px] w-4/5'>
-            <img className='' src={Speaker} alt="Speaker"/>
+        <div className='pt-[7px] w-3/5'>
+            <img className='' src={image} alt="Speaker"/>
         </div>
-        <p className='text-[12px] font-medium'>Headphones</p>
+        <div className='mt-3'>
+          <p className='text-[12px] leading-none font-medium'>{name}</p>
+        </div>
+       
     </div>
   )
 }
