6e2049055c88c7f30f2e65a01c224731b7508722
Prince <princemehta516@gmail.com>
2023-07-14 23:46:46 +0530

    add redux persist

--- frontend/src/components/RouteLayout.jsx
+++ frontend/src/components/RouteLayout.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import {Outlet} from 'react-router-dom';
-import Navbar from './Navbar';
-import store from '../store/store';
-import {Provider} from 'react-redux';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import { persistor, store } from "../store/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 const RouteLayout = () => {
   return (
     <>
-    <Provider store={store}>
-      <Navbar />
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <Navbar />
 
-      <main>
-        <Outlet />
-      </main>
+          <main>
+            <Outlet />
+          </main>
+        </PersistGate>
       </Provider>
     </>
-  )
-}
+  );
+};
 
-export default RouteLayout
+export default RouteLayout;
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -4,15 +4,55 @@ import bannerSlice from "./bannerSlice";
 import productSlice from './productSlice';
 import categorySlice from "./categorySlice";
 import cartSlice from "./cartSlice";
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 
-const store=configureStore({
+const productpersistConfig = {
+  key: 'product',
+  storage,
+}
+const bannerPersistConfig = {
+    key: 'banner',
+    storage,
+  }
+  const cartPersistConfig = {
+    key: 'cart',
+    storage,
+  }
+  const categoryPersistConfig = {
+    key: 'category',
+    storage,
+  }
+
+const persistedProudctReducer = persistReducer(productpersistConfig, productSlice)
+const persistedBannerReducer = persistReducer(bannerPersistConfig, bannerSlice)
+const persistCartReducer = persistReducer(cartPersistConfig, cartSlice)
+const persistedCategoryReducer = persistReducer(categoryPersistConfig, categorySlice)
+
+export const store=configureStore({
     reducer:
     {
-        banners:bannerSlice,
-        products:productSlice,
-        categories:categorySlice,
+        banners:persistedBannerReducer,
+        products:persistedProudctReducer,
+        categories:persistedCategoryReducer,
         cart:cartSlice,
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    })
 })
+export const persistor = persistStore(store)
 
-export default store;
+
