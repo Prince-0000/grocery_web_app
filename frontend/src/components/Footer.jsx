4e01e337544d30414ce5cd0a46aaffe1856dd202
Prince <princemehta516@gmail.com>
2023-07-29 14:30:31 +0530

    update responsiveness

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
   return (
     <div className='md:my-5 sm:my-4 my-3 flex text-center justify-center font-medium'>
         <p className='md:text-xl sm:text-base text-sm'>Copyright@2023 Dukaan</p>
+  
     </div>
   )
 }
